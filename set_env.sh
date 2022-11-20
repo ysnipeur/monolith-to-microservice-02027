e2a7f7b9048fe63a1f911d4b135123792505faae
yvan takoumbo <yvansnippeur@gmail.com>
2022-11-20 09:21:04 +0100

    Code deployed

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+#rien a dire
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres2022
+export POSTGRES_HOST=instancedbprojet3.cu59icumc0nj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramDB
+export AWS_BUCKET=yvan-udagram-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
